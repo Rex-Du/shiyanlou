3b312813a5bfa0d0e477cfc3ac6a68fc813f08bd
rexdu <860435288@qq.com>
2020-07-27 23:04:56 +0800

    新增10道算法

---
+++ free/leetcode/面试题02.02.py
@@ -0,0 +1,34 @@
+"""
+面试题02.02:实现一种算法，找出单向链表中倒数第 k 个节点。返回该节点的值。
+
+注意：本题相对原题稍作改动
+
+示例：
+
+输入： 1->2->3->4->5 和 k = 2
+输出： 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-node-from-end-of-list-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# author: rexdu
+# create: 2020/7/27 22:41
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def kthToLast(self, head: ListNode, k: int) -> int:
+        fast, slow = head, head
+        while k > 0:
+            fast = fast.next
+            k -= 1
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        return slow.val
+
+
---
+++ free/leetcode/面试题04.02.py
@@ -0,0 +1,43 @@
+"""
+面试题04.02:给定一个有序整数数组，元素各不相同且按升序排列，编写一个算法，创建一棵高度最小的二叉搜索树。
+
+示例:
+给定有序数组: [-10,-3,0,5,9],
+
+一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+
+          0
+         / \
+       -3   9
+       /   /
+     -10  5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-height-tree-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# author: rexdu
+# create: 2020/7/27 22:15
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+        n = nums.__len__()
+        mid = n // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
+
+
+
+
